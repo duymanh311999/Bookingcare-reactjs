2458a4282ad986b7301ee84c9b66ea1c33e7bef7
manh <duymanh311999@gmail.com>
2023-05-09 18:14:46 +0700

    bai 76

--- src/services/userService.js
+++ src/services/userService.js
@@ -50,9 +50,15 @@ const saveBulkScheduleDoctor = (data) => {
     return axios.post('/api/bulk-create-schedule', data)
 }
 
+const getScheduleDoctorByDate = (doctorId, date) => {
+    return axios.get(`/api/get-schedule-doctor-by-date?doctorId=${doctorId}&date=${date}`)
+}
+
+
 
 
 export {handleLoginApi, getAllUsers, creataNewUserServie, deleteUserService,
      editUserService, getAllCodeService, getTopDoctorHomeService, getAllDoctors, 
      saveDetailDoctorService, getDetailInforDoctor, saveBulkScheduleDoctor,
+     getScheduleDoctorByDate,
     }
