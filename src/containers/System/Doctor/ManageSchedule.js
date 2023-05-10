230e0fa8672eaacb9982d7e231cccb2b971e0296
manh <duymanh311999@gmail.com>
2023-05-10 12:08:27 +0700

    Bai 78

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -129,15 +129,18 @@ class ManageSchedule extends Component {
             doctorId:  selectedDoctor.value,
             formatedDate: formatedDate
        })
-
-       console.log("check saveBulkScheduleDoctor:",res)
-
-       console.log("check result:",result)
+       if(res && res.errCode === 0){
+            toast.success("Save inFor succeed!");
+       }else{
+            toast.error("error saveBulkScheduleDoctor");
+            console.log('error saveBulkScheduleDoctor:',res);
+       }
      }
 
     render() {
         let {rangeTime} = this.state;
         let {language} = this.props;
+        let yesterday = new Date(new Date().setDate(new Date().getDate()-1));
         return (  
             <div className='manage-schedule-container'>
                 <div className='m-s-title'>
@@ -159,7 +162,7 @@ class ManageSchedule extends Component {
                                 onChange={this.handleOnchangeDataPicker}
                                 className='form-control'
                                 value={this.state.currentDate}
-                                minDate={new Date()} 
+                                minDate={yesterday}
                              />
                         </div>
                         <div className='col-12 pick-hour-container'>
